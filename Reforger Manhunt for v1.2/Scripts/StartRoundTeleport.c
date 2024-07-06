af4e4a6dda87ddf56284fd1f713556a0a229cc6d
VampyrCat <arieuzf@gmail.com>
2024-07-06 19:35:13 +0300

    stuff

--- Reforger Manhunt for v1.2/Scripts/StartRoundTeleport.c
+++ Reforger Manhunt for v1.2/Scripts/StartRoundTeleport.c
@@ -1,3 +1,10 @@
+/*point of the teleport square
+	  	  x	    	z
+pos1 = 3431.564, 2825.148
+pos2 = 3279.132, 1199.893
+pos3 = 1617.603, 879.849
+pos4 = 1825.784, 3354.84*/
+
 class RandomAreaTeleport
 {
 	array<IEntity> players = {};
